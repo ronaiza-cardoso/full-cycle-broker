868961650ed3323c0cce91d8e8117ecb142633c4
Ronaiza Cardoso <ronaizacard@gmail.com>
2023-06-21 18:17:43 -0300

    chore: initial commit

---
+++ internal/market/entity/order_queue.go
@@ -0,0 +1,27 @@
+package entity
+
+type OrderQueue struct {
+	Orders []*Order
+}
+
+// Less - compare two values
+func (oq *OrderQueue) Less(i, j int) bool {
+	return oq.Orders[i].Price < oq.Orders[j].Price
+}
+
+// Swap - invert data value
+func (oq *OrderQueue) Swap(i, j int) {
+	oq.Orders[i], oq.Orders[j] = oq.Orders[j], oq.Orders[i]
+}
+
+// Len - verify data length
+func (oq *OrderQueue) Len() int {
+	return len(oq.Orders)
+}
+
+// Push - append value
+func (oq *OrderQueue) Push(x interface{}) {
+	oq.Orders = append(oq.Orders, x.(*Order))
+}
+
+// Pop - remove item at a given position
---
+++ todo.md
@@ -0,0 +1,4 @@
+(Aprenda Docker do Zero, tutorial passo a passo)[https://www.youtube.com/watch?v=8MJWke19g8U&ab_channel=FullCycle
+]
+
+go lang - https://www.youtube.com/watch?v=_MkQLDMak-4&list=PL5aY_NrL1rjucQqO21QH8KclsLDYu1BIg&ab_channel=FullCycle
